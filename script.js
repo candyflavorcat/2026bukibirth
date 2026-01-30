708dbd29bc3dd2634f763db6c9ef111e76a0b566
candyflavorcat <candyflavorcat@gmail.com>
2026-01-30 20:51:41 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-$(document).ready(function () {
-  $("#book").turn({
-    width: 900,
-    height: 1200,
-    autoCenter: true
-  });
-});
+const totalPages = 70;
 
+for(let i = 1; i <= totalPages; i++) {
+  $("#book").append(
+    `<img loading="lazy" src="pages/page${i}.jpg">`
+  );
+}
+
+$("#book").turn({
+  width: $("#book").width(),
+  height: $("#book").height(),
+  autoCenter: true
+});
