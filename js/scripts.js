5190570ab6e9e6e4cd1725096c7aac395456018c
candyflavorcat <candyflavorcat@gmail.com>
2026-01-30 22:46:45 +0900

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -32,20 +32,19 @@ $(document).ready(function() {
             });
 
             // --- 클릭 이벤트 추가 시작 ---
-        $page.on('click', function(e) {
-            // 현재 클릭한 지점이 페이지의 왼쪽 절반인지 오른쪽 절반인지 체크
-            const pageWidth = $(this).width();
-            const clickX = e.pageX - $(this).offset().left;
+            $page.on('click', function() {
+            // 현재 페이지가 짝수면 왼쪽 페이지(이전), 홀수면 오른쪽 페이지(다음)
+            // (첫 페이지가 1번인 double-page 모드 기준)
+                if (i % 2 === 0) {
+                    $book.turn('previous');
+                } else {
+                    $book.turn('next');
+                }
+            });
+            // --- 클릭 이벤트 추가 끝 ---
 
-            if (clickX > pageWidth / 2) {
-                // 오른쪽 클릭 시 다음 페이지로
-                $book.turn('next');
-            } else {
-                // 왼쪽 클릭 시 이전 페이지로
-                $book.turn('previous');
-            }
-        });
-        // --- 클릭 이벤트 추가 끝 ---
+            
+    
 
             // 해당 페이지에 북마크 설정이 있다면 추가
             if (FlipbookConfig.bookmarks[i]) {
